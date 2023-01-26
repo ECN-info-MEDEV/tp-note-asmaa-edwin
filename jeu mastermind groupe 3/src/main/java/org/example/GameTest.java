1effc53d62c8db6c51d335bb450830e05c204285
ASMAA-1 <73344426+ASMAA-1@users.noreply.github.com>
2023-01-26 11:23:04 +0100

    Update GameTest.java

--- jeu mastermind groupe 3/src/main/java/org/example/GameTest.java
+++ jeu mastermind groupe 3/src/main/java/org/example/GameTest.java
@@ -1,18 +1,39 @@
 package org.example;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class GameTest {
-
+public class GameTest {
     @Test
-    void codemakerTurn() {
-
-
+    public void testCodemakerTurn() {
+        Game game = new Game(false);
+        game.codemakerTurn();
+        // Vérifiez que le code secret a bien été défini
+        assertNotNull(game.secretCode);
+        // Vérifiez que le code secret contient 4 éléments
+        assertEquals(4, game.secretCode.length);
     }
-
+    
     @Test
-    void codebreakerTurn() {
+    public void testCodebreakerTurn() {
+        Game game = new Game(true);
+        game.codemakerTurn();
+        game.codebreakerTurn();
+        // Vérifiez que le score du Codebreaker est correctement incrémenté lorsqu'il a deviné le code secret
+        assertEquals(1, game.score[0]);
+        // Vérifiez que la partie est terminée lorsqu'il a deviné le code secret
+        assertTrue(game.gameOver);
+    }
+    
+    @Test
+    public void testPlay() {
+        Game game = new Game(true);
+        game.play();
+        // Vérifiez que la méthode codebreakerTurn() est appelée lorsque le joueur est le Codebreaker
+        assertEquals(1, game.score[0]);
+        game = new Game(false);
+        game.play();
+        // Vérifiez que la méthode codemakerTurn() est appelée lorsque le joueur est le CodeMaker
+        assertNotNull(game.secretCode);
     }
 }
